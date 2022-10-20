97f6d85bcdc6c0f1ac140b07b0df111652a8873b
jgfn1 <jgfn@cin.ufpe.br>
2022-10-20 11:50:25 -0300

    added mui button and moved the airport select logic to another component

--- src/App.tsx
+++ src/App.tsx
@@ -1,93 +1,46 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Autocomplete, TextField } from '@mui/material';
+import { Autocomplete, Button, TextField } from '@mui/material';
+import AirportSelect, { Airport } from './components/AirportSelect';
 
 function App() {
-  type Airport = {
-    name: string;
-    city: string;
-    code?: string;
-    icaoCode?: string;
-    latitude: number;
-    longitude: number;
-    label: string;
-  };
-
-  const [firstAirport, setFirstAirport] = useState({} as Airport);
-  const [secondAirport, setSecondAirport] = useState({} as Airport);
+  const [firstAirport, setFirstAirport] = useState({
+    label: 'Choose airport',
+  } as Airport);
+  const [secondAirport, setSecondAirport] = useState({
+    label: 'Choose airport',
+  } as Airport);
   const [distance, setDistance] = useState(0);
-  const [airports, setAirports] = useState([] as Airport[]);
-
-  function datToArray(text: string) {
-    const lines = text.split(/\n/); // split on \n
-    return lines
-      .filter(line => line.includes('United States'))
-      .map(line => {
-        const {
-          1: name,
-          2: city,
-          4: code,
-          5: icaoCode,
-          6: latitude,
-          7: longitude,
-        } = line.replaceAll('"', '').split(',');
-        return {
-          name,
-          city,
-          latitude: parseFloat(latitude),
-          longitude: parseFloat(longitude),
-          label: code !== '\\N' ? `${name} - ${code}` : `${name} - ${icaoCode}`,
-        } as Airport;
-      });
-  }
-
-  useEffect(() => {
-    axios
-      .get(
-        'https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat'
-      )
-      .then(response => {
-        const airportsArray = datToArray(response.data);
-        setAirports(airportsArray);
-        setFirstAirport(airportsArray[0]);
-        setSecondAirport(airportsArray[0]);
-      });
-  }, []);
 
   const onHandleCalculate = () => {
-    setDistance(distance + 1);
-    console.log('airports: ', airports);
+    console.log(
+      'SELECTED AIRPORTS:\n',
+      firstAirport.label,
+      '\n',
+      secondAirport.label
+    );
   };
 
   return (
     <>
       <h1>Insert USA Airports to Calculate Distance</h1>
-      <Autocomplete
-        value={firstAirport}
-        onChange={(event: any, newValue: Airport | null) => {
-          setFirstAirport(newValue || airports[0]);
-        }}
-        disablePortal
+      <AirportSelect
+        airport={firstAirport}
+        setAirport={setFirstAirport}
         id="first-airport-input"
-        options={airports}
-        sx={{ width: 400 }}
-        renderInput={params => <TextField {...params} label="1st Airport" />}
-      ></Autocomplete>
+        label="1st Airport"
+      ></AirportSelect>
       <br />
-      <Autocomplete
-        value={secondAirport}
-        onChange={(event: any, newValue: Airport | null) => {
-          setSecondAirport(newValue || airports[0]);
-        }}
-        disablePortal
+      <AirportSelect
+        airport={secondAirport}
+        setAirport={setSecondAirport}
         id="second-airport-input"
-        options={airports}
-        sx={{ width: 400 }}
-        renderInput={params => <TextField {...params} label="2nd Airport" />}
-      ></Autocomplete>
+        label="2nd Airport"
+      ></AirportSelect>
       <br />
-      <br />
-      <button onClick={onHandleCalculate}>Calculate</button>
+      <Button variant="contained" onClick={onHandleCalculate}>
+        Calculate
+      </Button>
       <br />
       <br />
       <label>Distance:</label>
---
+++ src/components/AirportSelect/index.tsx
@@ -0,0 +1,83 @@
+import { Autocomplete, TextField } from '@mui/material';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+type Airport = {
+  name: string;
+  city: string;
+  code?: string;
+  icaoCode?: string;
+  latitude: number;
+  longitude: number;
+  label: string;
+};
+
+interface AirportSelectProps {
+  airport: Airport;
+  setAirport: Function;
+  id: string;
+  label: string;
+}
+
+const AirportSelect = ({
+  airport,
+  setAirport,
+  id,
+  label,
+}: AirportSelectProps) => {
+  const [airports, setAirports] = useState([] as Airport[]);
+
+  function datToArray(text: string) {
+    const lines = text.split(/\n/); // split on \n
+    return lines
+      .filter(line => line.includes('United States'))
+      .map(line => {
+        const {
+          1: name,
+          2: city,
+          4: code,
+          5: icaoCode,
+          6: latitude,
+          7: longitude,
+        } = line.replaceAll('"', '').split(',');
+        return {
+          name,
+          city,
+          latitude: parseFloat(latitude),
+          longitude: parseFloat(longitude),
+          label: code !== '\\N' ? `${name} - ${code}` : `${name} - ${icaoCode}`,
+        } as Airport;
+      });
+  }
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat'
+      )
+      .then(response => {
+        const airportsArray = datToArray(response.data);
+        setAirports(airportsArray);
+        setAirport(airportsArray[0]);
+      });
+  }, []);
+
+  return (
+    <>
+      <Autocomplete
+        value={airport}
+        onChange={(event: any, newValue: Airport | null) => {
+          setAirport(newValue || airports[0]);
+        }}
+        disablePortal
+        id={id}
+        options={airports}
+        sx={{ width: 400 }}
+        renderInput={params => <TextField {...params} label={label} />}
+      ></Autocomplete>
+    </>
+  );
+};
+
+export default AirportSelect;
+export type { Airport };
