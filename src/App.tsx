711eae578191084def6fc46ddfe7841f05878c1d
jgfn1 <jgfn@cin.ufpe.br>
2022-10-24 14:16:22 -0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -2,63 +2,29 @@ import React, { useState } from 'react';
 import { Button, Grid, Typography } from '@mui/material';
 import AirportSelect, { Airport } from './components/AirportSelect';
 import { Container } from '@mui/system';
+import calculateDistanceInNmFromCoordinates from './utils/calculateDistanceInNmFromCoordinates';
 
 function App(): JSX.Element {
-  const [firstAirport, setFirstAirport] = useState<Airport>({
+  const chooseAirport = {
     label: 'Choose airport',
     name: '',
     city: '',
     latitude: 0,
     longitude: 0,
-  });
-  const [secondAirport, setSecondAirport] = useState<Airport>({
-    label: 'Choose airport',
-    name: '',
-    city: '',
-    latitude: 0,
-    longitude: 0,
-  });
-  const [distance, setDistance] = useState(0);
-
-  const calculateDistanceInKmFromCoordinates = (
-    lat1: number,
-    lon1: number,
-    lat2: number,
-    lon2: number
-  ): number => {
-    // The math module contains a function
-    // named toRadians which converts from
-    // degrees to radians.
-    lon1 = (lon1 * Math.PI) / 180;
-    lon2 = (lon2 * Math.PI) / 180;
-    lat1 = (lat1 * Math.PI) / 180;
-    lat2 = (lat2 * Math.PI) / 180;
-
-    // Haversine formula
-    const dlon = lon2 - lon1;
-    const dlat = lat2 - lat1;
-    const a =
-      Math.pow(Math.sin(dlat / 2), 2) +
-      Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dlon / 2), 2);
-
-    const c = 2 * Math.asin(Math.sqrt(a));
-
-    // Radius of earth in kilometers. Use 3956
-    // for miles
-    const r = 6371;
-
-    // calculate the result
-    return c * r;
   };
+
+  const [firstAirport, setFirstAirport] = useState<Airport>(chooseAirport);
+  const [secondAirport, setSecondAirport] = useState<Airport>(chooseAirport);
+  const [distance, setDistance] = useState(0);
 
   const onHandleCalculate = (): void => {
     setDistance(
-      calculateDistanceInKmFromCoordinates(
+      calculateDistanceInNmFromCoordinates(
         firstAirport.latitude,
         firstAirport.longitude,
         secondAirport.latitude,
         secondAirport.longitude
-      ) * 0.539957
+      )
     );
   };
 
