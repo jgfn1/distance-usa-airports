7b6e353f05b27c8793f15ac1e5a3abaa9f96bd3d
jgfn1 <jgfn@cin.ufpe.br>
2022-10-24 14:36:04 -0300

    adding back paper and typography to the result

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Grid, Typography } from '@mui/material';
+import { Button, Paper, Grid, Typography } from '@mui/material';
 import AirportSelect, { Airport } from './components/AirportSelect';
 import { Container } from '@mui/system';
 import calculateDistanceInNmFromCoordinates from './utils/calculateDistanceInNmFromCoordinates';
@@ -12,7 +12,6 @@ function App(): JSX.Element {
     latitude: 0,
     longitude: 0,
   };
-
   const [firstAirport, setFirstAirport] = useState<Airport>(chooseAirport);
   const [secondAirport, setSecondAirport] = useState<Airport>(chooseAirport);
   const [distance, setDistance] = useState(0);
@@ -39,56 +38,59 @@ function App(): JSX.Element {
           transform: 'translate(-50%, -50%)',
         }}
       >
-        <Grid
-          container
-          direction="column"
-          justifyContent="center"
-          alignItems="center"
-          spacing={4}
-        >
-          <Grid item>
-            <Typography variant="h4" align="center">
-              Insert Airports to Calculate Distance
-            </Typography>
-          </Grid>
-
+        <Paper elevation={3}>
           <Grid
-            item
             container
-            direction="row"
+            direction="column"
             justifyContent="center"
-            alignItems="space-around"
-            spacing={2}
+            alignItems="center"
+            spacing={4}
           >
-            <Grid item xs={8} md={5} lg={5} xl={4}>
-              <AirportSelect
-                airport={firstAirport}
-                setAirport={setFirstAirport}
-                id="first-airport-input"
-                label="1st Airport"
-              ></AirportSelect>
+            <Grid item>
+              <Typography variant="h4" align="center">
+                Insert Airports to Calculate Distance
+              </Typography>
             </Grid>
-            <Grid item xs={8} md={5} lg={5} xl={4}>
-              <AirportSelect
-                airport={secondAirport}
-                setAirport={setSecondAirport}
-                id="second-airport-input"
-                label="2nd Airport"
-              ></AirportSelect>
+
+            <Grid
+              item
+              container
+              direction="row"
+              justifyContent="center"
+              alignItems="space-around"
+              spacing={2}
+            >
+              <Grid item xs={8} md={5} lg={5} xl={4}>
+                <AirportSelect
+                  airport={firstAirport}
+                  setAirport={setFirstAirport}
+                  id="first-airport-input"
+                  label="1st Airport"
+                ></AirportSelect>
+              </Grid>
+              <Grid item xs={8} md={5} lg={5} xl={4}>
+                <AirportSelect
+                  airport={secondAirport}
+                  setAirport={setSecondAirport}
+                  id="second-airport-input"
+                  label="2nd Airport"
+                ></AirportSelect>
+              </Grid>
+            </Grid>
+
+            <Grid item>
+              <Button variant="contained" onClick={onHandleCalculate}>
+                Calculate
+              </Button>
+            </Grid>
+
+            <Grid item sx={{ pb: 5 }}>
+              <Typography variant="h5" align="center">
+                Straight Line Distance: {distance.toFixed(2)} Nautical Miles
+              </Typography>
             </Grid>
           </Grid>
-
-          <Grid item>
-            <Button variant="contained" onClick={onHandleCalculate}>
-              Calculate
-            </Button>
-          </Grid>
-
-          <Grid item>
-            <label>Straight Line Distance:</label>
-            <span> {distance.toFixed(2)} Nautical Miles</span>
-          </Grid>
-        </Grid>
+        </Paper>
       </Container>
     </>
   );
